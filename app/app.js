00523c82371bd3d0779dc6727d86c9a5a0278442
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-24 02:59:08 +0200

    Initial setup main app.js file

---
+++ app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    Stylesheet
+} from "react-native";
+
+export default class App extends Component {
+    render(){
+        return(
+            <Text>Hello World!</Text>
+        );
+    }
+}
--- index.android.js
+++ index.android.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import App from "./app/App";
 
 export default class shehui extends Component {
   render() {
     return (
-      <View>
-      </View>
+      <App />
     );
   }
 }
